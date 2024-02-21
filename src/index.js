35ec8e3477d4389a8ab2d119f77e125ae1445138
Prasann0009 <prasannlasod0009@gmail.com>
2024-02-21 14:58:47 +0530

    second upload

--- src/index.js
+++ src/index.js
@@ -2,20 +2,35 @@ import React,{useState} from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const colors = ["blue","red","black","orange"];
+// const colors = ["blue","red","black","orange"];
+
+// const App = ()=>{
+//       const [index,setColor] = useState(0);
+//      const changeColor = ()=>{
+//         let newIndex = index===colors.length-1?0:index+1;
+//         setColor(newIndex);
+//      }
+//        return (
+//         <div className="changecolor" style={{backgroundColor:colors[index],border:"5px solid black"}}>
+//             <button onClick={changeColor}>Change Color</button>
+//         </div>
+//        );
+// }
 
 const App = ()=>{
-      const [index,setColor] = useState(0);
-     const changeColor = ()=>{
-        let newIndex = index===colors.length-1?0:index+1;
-        setColor(newIndex);
-     }
-       return (
-        <div className="changecolor" style={{backgroundColor:colors[index],border:"5px solid black"}}>
-            <button onClick={changeColor}>Change Color</button>
-        </div>
-       );
+    
+    const [inputvalue,setInput] = useState("");
+    
+    const onChangeText = (event)=>{
+        setInput(event.target.value);
+    }
+    return (
+        <>
+        <label htmlFor="name">Enter your name:</label><br></br><br></br>
+        <input id="name" onChange={onChangeText}/>
+        {inputvalue==""?<p></p>:<p>{`Hello ${inputvalue}!`}</p>}
+        </>    
+    );
 }
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
